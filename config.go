288c9b025b8107408053224930ab8a6214220bd0
L. Jiang <l.jiang.1024@gmail.com>
2025-08-20 22:15:23 +0800

    feat: add yaml struct tag in Config

--- config.go
+++ config.go
@@ -2,6 +2,6 @@ package jwt
 
 // Config config struct
 type Config struct {
-	Key    string `json:"key"`    // 密钥
-	Expire string `json:"expire"` // 超时时长，1s,10m
+	Key    string `json:"key" yaml:"key"`       // 密钥
+	Expire string `json:"expire" yaml:"expire"` // 超时时长，1s,10m
 }
